4bccdba9e62fce74c0e7196666764b7a828e963b
Thomas Svedin <thomas@swedart.se>
2024-12-28 10:30:38 +0100

    Started to add after burner to the ship

--- src/ui/components/Ship.ts
+++ src/ui/components/Ship.ts
@@ -6,9 +6,15 @@ export class Ship extends Phaser.GameObjects.Sprite {
     // I would like to add a method to move the ship
     private acceleration: number = 10;
     private maxSpeed: number = 300;
+    private shipVec: Phaser.GameObjects.Graphics;
+    private shipTextureKey: string = "shipTexture";
+    private afterburner: Phaser.GameObjects.Graphics;
+    private afterburnerTextureKey: string = "afterburnerTexture";
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, 'ship');
+        this.shipVec = new Phaser.GameObjects.Graphics(scene);
+        this.afterburner = new Phaser.GameObjects.Graphics(scene);
         this.scene = scene;
         this.speed = 100;
         
@@ -16,7 +22,7 @@ export class Ship extends Phaser.GameObjects.Sprite {
         
         // Disable debug rendering for the ship's body
         if (this.body instanceof Phaser.Physics.Arcade.Body) {
-            this.body.setCollideWorldBounds(true);
+            this.body.setCollideWorldBounds(false);
         }
 
         // Set up keyboard input
@@ -26,30 +32,32 @@ export class Ship extends Phaser.GameObjects.Sprite {
     }
 
     update(): void {
-        this.moveShip();    
+        this.moveShip();
+        this.wrapAroundScreen();    
     }
 
     protected create() {
-        const shipVec = this.drawShip();
+        this.shipVec = this.drawShip();
+        this.afterburner = this.drawAfterburner();
     
         // Generate a larger texture to include the entire ship
-        const shipTextureKey = 'shipTexture';
-        shipVec.generateTexture(shipTextureKey, 40, 40); // Increased size
-        this.setTexture(shipTextureKey);
-        shipVec.destroy();
+        this.shipVec.generateTexture(this.shipTextureKey, 40, 40);
+        this.setTexture(this.shipTextureKey);
+
+        this.afterburner.generateTexture(this.afterburnerTextureKey, 40, 40);
+        
     
         // Add the ship to the scene
-        //this.scene.add.existing(shipVec);
+        //this.scene.add.existing(this.shipVec);
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this);
         this.scene.physics.world.enable(this);
     }
 
     protected drawShip(): Phaser.GameObjects.Graphics {
-        const shipVec = new Phaser.GameObjects.Graphics(this.scene);
-        // Set shipVec x and y to the center of the screen
-        shipVec.x = this.scene.cameras.main.width / 2;
-        shipVec.y = this.scene.cameras.main.height / 2;
+        const shipGraphics = new Phaser.GameObjects.Graphics(this.scene);
+        shipGraphics.x = this.scene.cameras.main.width / 2;
+        shipGraphics.y = this.scene.cameras.main.height / 2;
     
         // Define the points of the ship
         const shipPoints = [
@@ -61,27 +69,65 @@ export class Ship extends Phaser.GameObjects.Sprite {
         ];
     
         // Draw the ship
-        shipVec.lineStyle(2, 0xffffff);
-        shipVec.beginPath();
-        shipVec.moveTo(shipPoints[0].x + 20, shipPoints[0].y + 20); // Offset points
+        shipGraphics.lineStyle(2, 0xffffff);
+        shipGraphics.beginPath();
+        shipGraphics.moveTo(shipPoints[0].x + 20, shipPoints[0].y + 20); // Offset points
         for (let i = 1; i < shipPoints.length; i++) {
-            shipVec.lineTo(shipPoints[i].x + 20, shipPoints[i].y + 20); // Offset points
+            shipGraphics.lineTo(shipPoints[i].x + 20, shipPoints[i].y + 20); // Offset points
         }
-        shipVec.closePath();
-        shipVec.strokePath();
+        shipGraphics.closePath();
+        shipGraphics.strokePath();
     
         // Add a dot to the ship's center to make it easier to see the ship's position
-        shipVec.fillStyle(0xffffff);
-        shipVec.fillCircle(20, 20, 2); // Offset center
+        shipGraphics.fillStyle(0xffffff);
+        shipGraphics.fillCircle(20, 20, 2); // Offset center
 
-        return shipVec;
+        return shipGraphics;
     }
 
-    public moveShip(): void {
+    protected drawAfterburner(): Phaser.GameObjects.Graphics {
+        const afterburner = new Phaser.GameObjects.Graphics(this.scene);
+        afterburner.x = this.shipVec.x;
+        afterburner.y = this.shipVec.y;
+    
+        // Define the points of the afterburner
+        const afterburnerPoints = [
+            { x: 0, y: 10 },
+            { x: 5, y: 15 },
+            { x: 0, y: 20 },
+            { x: -5, y: 15 },
+            { x: 0, y: 10 }
+        ];
+    
+        // Draw the afterburner
+        afterburner.lineStyle(2, 0x666666);
+        afterburner.beginPath();
+        afterburner.moveTo(afterburnerPoints[0].x + 20, afterburnerPoints[0].y + 20); // Offset points
+        for (let i = 1; i < afterburnerPoints.length; i++) {
+            afterburner.lineTo(afterburnerPoints[i].x + 20, afterburnerPoints[i].y + 20); // Offset points
+        }
+        afterburner.closePath();
+        afterburner.strokePath();
+    
+        return afterburner;
+    }
+
+    protected addAfterburner(): void {
+        this.afterburner.x = this.x - this.width / 2;
+        this.afterburner.y = this.y - this.height / 2;
+        this.scene.add.existing(this.afterburner);
+    }
+    
+    protected removeAfterburner(): void {
+        this.scene.children.remove(this.afterburner);
+    }
+
+    protected moveShip(): void {
         if (this.body) {
             const body = this.body as Phaser.Physics.Arcade.Body;
 
             if (this.cursors.up?.isDown) {
+                this.addAfterburner();
                 const currentVelocity = new Phaser.Math.Vector2(body.velocity.x, body.velocity.y);
                 const angleInRadians = Phaser.Math.DegToRad(this.angle - 90);
                 const accelerationVector = new Phaser.Math.Vector2(
@@ -96,6 +142,7 @@ export class Ship extends Phaser.GameObjects.Sprite {
 
                 body.setVelocity(currentVelocity.x, currentVelocity.y);
             } else {
+                this.removeAfterburner();
                 body.setVelocity(body.velocity.x * 0.99, body.velocity.y * 0.99); // Apply some drag when not accelerating
             }
 
@@ -104,6 +151,36 @@ export class Ship extends Phaser.GameObjects.Sprite {
             } else if (this.cursors.right?.isDown) {
                 this.angle += 5; // Continue rotating clockwise
             }
+        }
+    }
+
+    private wrapAroundScreen(): void {
+        if (this.body) {
+            const body = this.body as Phaser.Physics.Arcade.Body;
+            const screenWidth = this.scene.scale.width;
+            const screenHeight = this.scene.scale.height;
+
+            if (body.x < 0 - body.height) {
+                body.x = screenWidth;
+            } else if (body.x > screenWidth + body.height) {
+                body.x = 0;
+                body.x -= body.height;
+            }
+
+            if (body.y < 0 - body.height) {
+                body.y = screenHeight;
+            } else if (body.y > screenHeight + body.height) {
+                body.y = 0;
+                body.y -= body.height;
+            }
+
+            /*if (body.y < 0) {
+                body.y = screenHeight;
+                body.y -= this.height; // Adjust position to account for ship's height
+            } else if (body.y > screenHeight) {
+                body.y = 0;
+                body.y += this.height; // Adjust position to account for ship's height
+            }*/
         }
     }
 }
