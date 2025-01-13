68f85cef6ab4fabfbdc755789e8cdda7a284a2ae
Thomas Svedin <thomas@swedart.se>
2025-01-13 21:04:21 +0100

    refactoring to get rid of scope issues

--- src/ui/components/GameView.tsx
+++ src/ui/components/GameView.tsx
@@ -17,6 +17,7 @@ export default defineComponent({
   setup() {
     const score = ref(0);
     const lives = ref(0);
+    const gameOver = ref(false);
 
     const services: IUIServices = inject(servicesKey) as IUIServices;
     const uiEventManager: UIEventManager = services.uiEventManager;
@@ -26,6 +27,51 @@ export default defineComponent({
     let enemies: Phaser.GameObjects.Group;
     let bullets: Phaser.GameObjects.Group;
     let livesPanel: LivesPanel;
+    let currentScene: Phaser.Scene;
+
+    function newLife(scene: Phaser.Scene) {
+      ship = new Ship(scene, scene.cameras.main.width / 2, scene.cameras.main.height / 2);
+    }
+
+    function addColliders(scene: Phaser.Scene) {
+      scene.physics.add.collider(ship, enemies, function (this: Phaser.Scene, ship, astroid) {
+        console.log('Collision detected between ship and astroid');
+        const position = new Phaser.Math.Vector2((ship as Phaser.GameObjects.Sprite).x, (ship as Phaser.GameObjects.Sprite).y);
+        const angle = (ship as Phaser.GameObjects.Sprite).angle;
+        ship.destroy();
+        uiEventManager.dispatchEvent(GameEvents.ShipCollisionEvent);
+
+        explosion = new Explosion(this, position.x, position.y, angle);
+        handleAstroidHitEnemy.call(this, astroid as Phaser.GameObjects.Sprite);
+      }, undefined, scene);
+
+      scene.physics.add.collider(bullets, enemies, function (this: Phaser.Scene, bullet, astroid) {
+        handleAstroidHitEnemy.call(this, astroid as Phaser.GameObjects.Sprite);
+        bullet.destroy();
+      }, undefined, scene);
+    }
+
+    function handleAstroidHitEnemy(this: Phaser.Scene, astroid: Phaser.GameObjects.Sprite) {
+      const sizeType = (astroid as Astroid).sizeType;
+      const position = new Phaser.Math.Vector2(astroid.x, astroid.y);
+      const astroidID = (astroid as Astroid).id;
+
+      uiEventManager.dispatchEvent(GameEvents.HitEvent, { enemy: sizeType });
+
+      astroid.destroy();
+      if (sizeType > 1) {
+        createSmallerEnemies.call(this, sizeType, position, astroidID);
+      }
+    }
+
+    function createSmallerEnemies(this: Phaser.Scene, sizeType: number, position: Phaser.Math.Vector2, parentID: string) {
+      const newSizeType = sizeType - 1;
+      const newEnemies = 2;
+      for (let i = 0; i < newEnemies; i++) {
+        const newAstroid = new Astroid(this, position.x, position.y, parentID + "-" + i, newSizeType);
+        enemies.add(newAstroid);
+      }
+    }
 
     onMounted(() => {
       const config: Phaser.Types.Core.GameConfig = {
@@ -41,7 +87,7 @@ export default defineComponent({
         },
         scene: {
           create: create,
-          update: update
+          update: update,
         },
         input: {
           keyboard: true,
@@ -54,16 +100,13 @@ export default defineComponent({
       new Phaser.Game(config);
 
       function update(this: Phaser.Scene) {
-        // Update logic here
-        /*if(background){
-          background.update();
-        }*/
         if (ship) {
           ship.update();
         }
       }
 
       function create(this: Phaser.Scene) {
+        currentScene = this;
         this.events.on('shoot', (bullet: Bullet) => {
           bullets.add(bullet);
         });
@@ -71,7 +114,7 @@ export default defineComponent({
         new Background(this, 0, 0);
         livesPanel = new LivesPanel(this, this.cameras.main.width - 100, 0);
 
-        ship = new Ship(this, this.cameras.main.width / 2, this.cameras.main.height / 2)
+        //ship = new Ship(this, this.cameras.main.width / 2, this.cameras.main.height / 2)
 
         bullets = this.add.group({
           classType: Bullet,
@@ -85,27 +128,12 @@ export default defineComponent({
 
         createEnemy(this);
 
-        // Add collision detection between ship and astroids
-        this.physics.add.collider(ship, enemies, function (this: Phaser.Scene, ship, astroid) {
-          console.log('Collision detected between ship and astroid');
-          const position = new Phaser.Math.Vector2((ship as Phaser.GameObjects.Sprite).x, (ship as Phaser.GameObjects.Sprite).y);
-          const angle = (ship as Phaser.GameObjects.Sprite).angle;
-          ship.destroy();
-          uiEventManager.dispatchEvent(GameEvents.ShipCollisionEvent);
-
-          explosion = new Explosion(this, position.x, position.y, angle);
-          handleAstroidHitEnemy.call(this, astroid as Phaser.GameObjects.Sprite);
-        }, undefined, this);
-
-        // Add collider between bullets and enemies with a callback function
-        this.physics.add.collider(bullets, enemies, function (this: Phaser.Scene, bullet, astroid) {
-          handleAstroidHitEnemy.call(this, astroid as Phaser.GameObjects.Sprite);
-          bullet.destroy();
-        }, undefined, this);
+        
 
         uiEventManager.dispatchEvent(GameEvents.GameSetupComplete);
-
       }
+
+      
 
       function createEnemy(scene: Phaser.Scene) {
         for (let i = 0; i < 4; i++) {
@@ -113,14 +141,13 @@ export default defineComponent({
           let y = Math.random() * scene.cameras.main.height;
           const centerX = scene.cameras.main.width / 2;
           const centerY = scene.cameras.main.height / 2;
-          const distanceFromCenter = 200; // Minimum distance from the center
+          const distanceFromCenter = 200;
 
           if (Math.abs(x - centerX) < distanceFromCenter && Math.abs(y - centerY) < distanceFromCenter) {
-            i--; // Retry if the position is too close to the center
+            i--;
             continue;
           }
 
-          //Round to the nearest 10
           x = Math.round(x / 10) * 10;
           y = Math.round(y / 10) * 10;
 
@@ -129,27 +156,7 @@ export default defineComponent({
         }
       }
 
-      function handleAstroidHitEnemy(this: Phaser.Scene, astroid: Phaser.GameObjects.Sprite) {
-        const sizeType = (astroid as Astroid).sizeType;
-        const position = new Phaser.Math.Vector2(astroid.x, astroid.y);
-        const astroidID = (astroid as Astroid).id;
-
-        uiEventManager.dispatchEvent(GameEvents.HitEvent, { enemy: sizeType });
-
-        astroid.destroy();  // Destroy the enemy
-        if (sizeType > 1) {
-          createSmallerEnemies.call(this, sizeType, position, astroidID);
-        }
-      }
-
-      function createSmallerEnemies(this: Phaser.Scene, sizeType: number, position: Phaser.Math.Vector2, parentID: string) {
-        const newSizeType = sizeType - 1;
-        const newEnemies = 2; // Create 2 new enemies
-        for (let i = 0; i < newEnemies; i++) {
-          const newAstroid = new Astroid(this, position.x, position.y, parentID + "-" + i, newSizeType);
-          enemies.add(newAstroid);
-        }
-      }
+      
     });
 
     watch(uiDataManager.game.score.ref, (newValue) => {
@@ -160,6 +167,12 @@ export default defineComponent({
     watch(uiDataManager.game.lives.ref, (newValue) => {
       lives.value = newValue;
       livesPanel.setLives(newValue);
+
+      if (newValue > 0) {
+        newLife(currentScene); // Use the stored scene instance
+        addColliders(currentScene);
+
+      }
     });
 
     return () => (
