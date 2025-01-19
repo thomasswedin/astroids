6e4b1b2d73a8d5432bc698847e94709585d9aafc
Thomas Svedin <thomas@swedart.se>
2025-01-19 20:20:21 +0100

    Refactor GameView and implement LevelEngine for improved level management and enemy handling

--- src/ui/components/GameView.tsx
+++ src/ui/components/GameView.tsx
@@ -5,14 +5,10 @@ import { servicesKey } from '../../UIController';
 import { UIDataManager } from "../data/UIDataManager";
 import { GameEvents } from '../events/game/GameEvents';
 import { UIEventManager } from "../events/ui/UIEventManager";
-import { Astroid } from "./Astroid";
 import { Background } from "./Background";
-import { Bullet } from './Bullet';
-import { EnemyShip } from './EnemyShip';
-import { Explosion } from './Explosion';
+import { LevelEngine } from './LevelEngine';
 import { LevelFactory } from './LevelFactory';
 import { LivesPanel } from './LivesPanel';
-import { Ship } from "./Ship";
 
 export default defineComponent({
   name: 'GameView',
@@ -24,75 +20,10 @@ export default defineComponent({
     const services: IUIServices = inject(servicesKey) as IUIServices;
     const uiEventManager: UIEventManager = services.uiEventManager;
     const uiDataManager: UIDataManager = services.dataManager;
-    let ship: Ship;
-    let explosion: Explosion;
-    let enemies: Phaser.GameObjects.Group;
-    let bullets: Phaser.GameObjects.Group;
     let livesPanel: LivesPanel;
     let currentScene: Phaser.Scene;
-    let enemyShip: EnemyShip;
     let levelFactory: LevelFactory;
-
-    function newLife(scene: Phaser.Scene) {
-      ship = new Ship(scene, scene.cameras.main.width / 2, scene.cameras.main.height / 2);
-    }
-
-    function createNewEnemy(scene: Phaser.Scene) {
-      enemyShip = new EnemyShip(scene, -120, scene.cameras.main.height / 4, ship, "enemyShip", 3);
-    }
-
-    function addColliders(scene: Phaser.Scene) {
-      scene.physics.add.collider(ship, enemies, function (this: Phaser.Scene, ship, astroid) {
-        console.log('Collision detected between ship and astroid');
-        const position = new Phaser.Math.Vector2((ship as Phaser.GameObjects.Sprite).x, (ship as Phaser.GameObjects.Sprite).y);
-        const angle = (ship as Phaser.GameObjects.Sprite).angle;
-        ship.destroy();
-        uiEventManager.dispatchEvent(GameEvents.ShipCollisionEvent);
-
-        explosion = new Explosion(this, position.x, position.y, angle);
-        handleAstroidHitEnemy.call(this, astroid as Phaser.GameObjects.Sprite);
-      }, undefined, scene);
-
-      scene.physics.add.collider(bullets, enemies, function (this: Phaser.Scene, bullet, astroid) {
-        if ((bullet as Bullet).bulletType === Bullet.PLAYER) {
-          handleAstroidHitEnemy.call(this, astroid as Phaser.GameObjects.Sprite);
-          bullet.destroy();
-        }
-      }, undefined, scene);
-
-      scene.physics.add.collider(bullets, ship, function (this: Phaser.Scene, bullet, ship) {
-        console.log('Collision detected between ship and bullet');
-        const position = new Phaser.Math.Vector2((ship as Phaser.GameObjects.Sprite).x, (ship as Phaser.GameObjects.Sprite).y);
-        const angle = (ship as Phaser.GameObjects.Sprite).angle;
-        bullet.destroy();
-        ship.destroy();
-        uiEventManager.dispatchEvent(GameEvents.ShipCollisionEvent);
-
-        explosion = new Explosion(this, position.x, position.y, angle);
-      }, undefined, scene);
-    }
-
-    function handleAstroidHitEnemy(this: Phaser.Scene, astroid: Phaser.GameObjects.Sprite) {
-      const sizeType = (astroid as Astroid).sizeType;
-      const position = new Phaser.Math.Vector2(astroid.x, astroid.y);
-      const astroidID = (astroid as Astroid).id;
-
-      uiEventManager.dispatchEvent(GameEvents.HitEvent, { enemy: sizeType });
-
-      astroid.destroy();
-      if (sizeType > 1) {
-        createSmallerEnemies.call(this, sizeType, position, astroidID);
-      }
-    }
-
-    function createSmallerEnemies(this: Phaser.Scene, sizeType: number, position: Phaser.Math.Vector2, parentID: string) {
-      const newSizeType = sizeType - 1;
-      const newEnemies = 2;
-      for (let i = 0; i < newEnemies; i++) {
-        const newAstroid = new Astroid(this, position.x, position.y, parentID + "-" + i, newSizeType);
-        enemies.add(newAstroid);
-      }
-    }
+    let levelEngine: LevelEngine;
 
     onMounted(() => {
       const config: Phaser.Types.Core.GameConfig = {
@@ -120,60 +51,20 @@ export default defineComponent({
 
       new Phaser.Game(config);
 
-      function update(this: Phaser.Scene) {
-        if (ship) {
-          ship.update();
-        }
-        if (enemyShip) {
-          enemyShip.update();
-        }
-      }
-
       function create(this: Phaser.Scene) {
         currentScene = this;
-
-        levelFactory = new LevelFactory(3);
-
-        currentScene.events.on('shoot', (bullet: Bullet) => {
-          bullets.add(bullet);
-        });
-
         new Background(currentScene, 0, 0);
         livesPanel = new LivesPanel(currentScene, currentScene.cameras.main.width - 100, 0);
+        levelFactory = new LevelFactory(3);
+        levelEngine = new LevelEngine(currentScene, levelFactory, uiEventManager);
 
-        bullets = currentScene.add.group({
-          classType: Bullet,
-          runChildUpdate: true
-        });
-
-        enemies = currentScene.add.group({
-          classType: Astroid,
-          runChildUpdate: true,
-        });
-
-        createAstroids(currentScene);
-
+        levelEngine.create();
         uiEventManager.dispatchEvent(GameEvents.GameSetupComplete);
       }
 
-      function createAstroids(scene: Phaser.Scene) {
-        for (let i = 0; i < levelFactory.getAmountOfAstroids(); i++) {
-          let x = Math.random() * scene.cameras.main.width;
-          let y = Math.random() * scene.cameras.main.height;
-          const centerX = scene.cameras.main.width / 2;
-          const centerY = scene.cameras.main.height / 2;
-          const distanceFromCenter = 200;
-
-          if (Math.abs(x - centerX) < distanceFromCenter && Math.abs(y - centerY) < distanceFromCenter) {
-            i--;
-            continue;
-          }
-
-          x = Math.round(x / 10) * 10;
-          y = Math.round(y / 10) * 10;
-
-          const enemy = new Astroid(scene, x, y, "astroid" + i, 3);
-          enemies.add(enemy);
+      function update() {
+        if (levelEngine) {
+          levelEngine.update();
         }
       }
     });
@@ -188,13 +79,9 @@ export default defineComponent({
       livesPanel.setLives(newValue);
 
       if (newValue > 0) {
-        newLife(currentScene); // Use the stored scene instance
-        addColliders(currentScene);
-
+        levelEngine.newLife(currentScene);
       }
-
-      createNewEnemy(currentScene);
-
+      levelEngine.createNewEnemy(currentScene);
     });
 
     return () => (
--- src/ui/components/LevelEngine.ts
+++ src/ui/components/LevelEngine.ts
@@ -1,6 +1,139 @@
+import Phaser from 'phaser';
+import { GameEvents } from '../events/game/GameEvents';
+import { UIEventManager } from "../events/ui/UIEventManager";
+import { Astroid } from './Astroid';
+import { Bullet } from './Bullet';
+import { EnemyShip } from './EnemyShip';
+import { Explosion } from './Explosion';
+import { LevelFactory } from './LevelFactory';
+import { Ship } from "./Ship";
+
 export class LevelEngine {
-    
-    constructor() {
-        
+
+    protected _currentScene: Phaser.Scene;
+    protected _levelFactory!: LevelFactory;
+    protected _ship!: Ship;
+    protected _explosion!: Explosion;
+    protected _enemies!: Phaser.GameObjects.Group;
+    protected _bullets!: Phaser.GameObjects.Group;
+    protected _enemyShip!: EnemyShip;
+    protected _uiEventManager!: UIEventManager;
+
+    constructor(scene: Phaser.Scene, levelFactory: LevelFactory, uiEventManager: UIEventManager) {
+        this._currentScene = scene;
+        this._levelFactory = levelFactory;
+        this._uiEventManager = uiEventManager;
+    }
+
+    public create() {
+        this._currentScene.events.on('shoot', (bullet: Bullet) => {
+            this._bullets.add(bullet);
+        });
+
+        this._bullets = this._currentScene.add.group({
+            classType: Bullet,
+            runChildUpdate: true
+        });
+
+        this._enemies = this._currentScene.add.group({
+            classType: Astroid,
+            runChildUpdate: true,
+        });
+
+        this.createAstroids(this._currentScene);
+    }
+
+    public update() {
+        if (this._ship) {
+            this._ship.update();
+        }
+        if (this._enemyShip) {
+            this._enemyShip.update();
+        }
+    }
+
+
+    public newLife(scene: Phaser.Scene) {
+        this._ship = new Ship(scene, scene.cameras.main.width / 2, scene.cameras.main.height / 2);
+        this.addColliders();
+        this.createNewEnemy(this._currentScene);
+    }
+
+    public createNewEnemy(scene: Phaser.Scene) {
+        this._enemyShip = new EnemyShip(scene, -120, scene.cameras.main.height / 4, this._ship, "enemyShip", 3);
+    }
+
+    protected createAstroids(scene: Phaser.Scene) {
+        for (let i = 0; i < this._levelFactory.getAmountOfAstroids(); i++) {
+            let x = Math.random() * scene.cameras.main.width;
+            let y = Math.random() * scene.cameras.main.height;
+            const centerX = scene.cameras.main.width / 2;
+            const centerY = scene.cameras.main.height / 2;
+            const distanceFromCenter = 200;
+
+            if (Math.abs(x - centerX) < distanceFromCenter && Math.abs(y - centerY) < distanceFromCenter) {
+                i--;
+                continue;
+            }
+
+            x = Math.round(x / 10) * 10;
+            y = Math.round(y / 10) * 10;
+
+            const enemy = new Astroid(scene, x, y, "astroid" + i, 3);
+            this._enemies.add(enemy);
+        }
+    }
+
+    protected handleAstroidHitEnemy(this: LevelEngine, astroid: Phaser.GameObjects.Sprite) {
+        const sizeType = (astroid as Astroid).sizeType;
+        const position = new Phaser.Math.Vector2(astroid.x, astroid.y);
+        const astroidID = (astroid as Astroid).id;
+
+        this._uiEventManager.dispatchEvent(GameEvents.HitEvent, { enemy: sizeType });
+
+        astroid.destroy();
+        if (sizeType > 1) {
+            this.createSmallerEnemies.call(this, sizeType, position, astroidID);
+        }
+    }
+
+    protected createSmallerEnemies(this: LevelEngine, sizeType: number, position: Phaser.Math.Vector2, parentID: string) {
+        const newSizeType = sizeType - 1;
+        const newEnemies = 2;
+        for (let i = 0; i < newEnemies; i++) {
+            const newAstroid = new Astroid(this._currentScene, position.x, position.y, parentID + "-" + i, newSizeType);
+            this._enemies.add(newAstroid);
+        }
+    }
+
+    protected addColliders() {
+        this._currentScene.physics.add.collider(this._ship, this._enemies, (ship, astroid) => {
+            console.log('Collision detected between ship and astroid');
+            const position = new Phaser.Math.Vector2((ship as Phaser.GameObjects.Sprite).x, (ship as Phaser.GameObjects.Sprite).y);
+            const angle = (ship as Phaser.GameObjects.Sprite).angle;
+            ship.destroy();
+            this._uiEventManager.dispatchEvent(GameEvents.ShipCollisionEvent);
+
+            this._explosion = new Explosion(this._currentScene, position.x, position.y, angle);
+            this.handleAstroidHitEnemy(astroid as Phaser.GameObjects.Sprite);
+        }, undefined, this._currentScene);
+
+        this._currentScene.physics.add.collider(this._bullets, this._enemies, (bullet, astroid) => {
+            if ((bullet as Bullet).bulletType === Bullet.PLAYER) {
+                this.handleAstroidHitEnemy(astroid as Phaser.GameObjects.Sprite);
+                bullet.destroy();
+            }
+        }, undefined, this._currentScene);
+
+        this._currentScene.physics.add.collider(this._bullets, this._ship, (bullet, ship) => {
+            console.log('Collision detected between ship and bullet');
+            const position = new Phaser.Math.Vector2((ship as Phaser.GameObjects.Sprite).x, (ship as Phaser.GameObjects.Sprite).y);
+            const angle = (ship as Phaser.GameObjects.Sprite).angle;
+            bullet.destroy();
+            ship.destroy();
+            this._uiEventManager.dispatchEvent(GameEvents.ShipCollisionEvent);
+
+            this._explosion = new Explosion(this._currentScene, position.x, position.y, angle);
+        }, undefined, this._currentScene);
     }
 }
